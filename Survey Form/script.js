7c708bed6d4118d92432c103781a4256fcdebca5
Akashpreet <101885362+Akashpreet@users.noreply.github.com>
2023-08-28 00:58:06 +0530

    Survey form uploaded

---
+++ Survey Form/script.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+  $("#tabs").tabs();
+  $(document).tooltip();
+  $("#btnSave").click(createListItem);
+  $("#GEN_4").datepicker({ dateFormat: "yy-mm-dd" }).val();
+});
+
+
+function createListItem() {
+ listCreationInformation = new SP.ListItemCreationInformation(); 
+ listItem = list.addItem(listCreationInformation);
+
+ var input_list = $('input');
+
+ var id, name, text;
+
+ for (var i = 0; i < input_list.length; i++) {
+     name = $(input_list[i]).attr('name');
+
+     if ($(input_list[i]).attr('type') === 'radio') {
+         //text = $(input_list[i]); //this code should be added to in some way!
+
+          // I have tried these versions, and none of them will work.
+
+         // text =$(input_list[i]).is(':checked');
+        // text = $(input_list[i]).prop('checked');
+         //text = $(input_list[i].checked('type') & 'radio');
+
+     }
+     else {
+         text = $(input_list[i]).val();
+     }
+     listItem.set_item(name, text);
+  }
+  listItem.update(); //Update the List Item
+  clientContext.executeQueryAsync(
+    Function.createDelegate(this, success),
+    Function.createDelegate(this, fail)
+   );
+ }
+ 
